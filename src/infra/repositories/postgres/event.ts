4235040d0d79a7be4ac8aa662e3b90f277d5e6c7
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-16 16:38:15 -0300

    refactor: move code production its own files

---
+++ src/infra/repositories/postgres/event.ts
@@ -0,0 +1,23 @@
+import { type GetEvent } from '@/domain/contracts/repos'
+import prisma from '@/infra/repositories/postgres/helpers/connection'
+
+export class PgEventRepository implements GetEvent {
+  async get ({ id }: GetEvent.Input): Promise<GetEvent.Output> {
+    const event = await prisma.event.findFirst({
+      where: {
+        id
+      },
+      select: {
+        id: true,
+        price: true
+      }
+    })
+
+    if (event !== null) {
+      return {
+        id: event.id,
+        price: event.price.toString()
+      }
+    }
+  }
+}
--- tests/infra/repositories/postres/event.spec.ts
+++ tests/infra/repositories/postres/event.spec.ts
@@ -1,27 +1,5 @@
-import { type GetEvent } from '@/domain/contracts/repos'
 import { prismaMock } from '@/tests/infra/repositories/postres/helpers'
-import prisma from '@/infra/repositories/postgres/helpers/connection'
-
-class PgEventRepository implements GetEvent {
-  async get ({ id }: GetEvent.Input): Promise<GetEvent.Output> {
-    const event = await prisma.event.findFirst({
-      where: {
-        id
-      },
-      select: {
-        id: true,
-        price: true
-      }
-    })
-
-    if (event !== null) {
-      return {
-        id: event.id,
-        price: event.price.toString()
-      }
-    }
-  }
-}
+import { PgEventRepository } from '@/infra/repositories/postgres'
 
 describe('PgEventRepository', () => {
   const sut = new PgEventRepository()
