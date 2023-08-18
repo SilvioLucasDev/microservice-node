013596537243e5fe5e000558a3606eb29f74a357
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-18 19:03:40 -0300

    test: ensure ExpressRouter calls handle with corect request

---
+++ tests/main/adapters/express-router.spec.ts
@@ -0,0 +1,18 @@
+import { ExpressRouter } from '@/main/adapters'
+import { type PurchaseTicketController } from '@/presentation/controllers'
+
+import { getMockReq, getMockRes } from '@jest-mock/express'
+import { mock } from 'jest-mock-extended'
+
+describe('ExpressRouter', () => {
+  it('should call handle with correct request', async () => {
+    const req = getMockReq({ body: { any: 'any' } })
+    const { res } = getMockRes()
+    const controller = mock<PurchaseTicketController>()
+    const sut = new ExpressRouter(controller)
+
+    await sut.adapt(req, res)
+
+    expect(controller.handle).toHaveBeenCalledWith({ any: 'any' })
+  })
+})
