ef6169f8615d39708825710a55116cf6f80eec48
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-18 00:37:53 -0300

    test: ensure PurchaseTicketController return 500 if purchase ticket throws

--- tests/presentation/controllers/purchase-ticket.spec.ts
+++ tests/presentation/controllers/purchase-ticket.spec.ts
@@ -4,12 +4,21 @@ import { type PurchaseTicket } from '@/application/use-cases'
 import { mock, type MockProxy } from 'jest-mock-extended'
 import { EventNotFoundError } from '@/domain/errors'
 
+export class ServerError extends Error {
+  constructor (error?: Error | undefined) {
+    super('Server failed. Try again soon')
+    this.name = 'ServerError'
+    this.stack = error?.stack
+  }
+}
+
 describe('PurchaseTicketController', () => {
   let sut: PurchaseTicketController
   let purchaseTicket: MockProxy<PurchaseTicket>
 
   beforeAll(() => {
     purchaseTicket = mock()
+    purchaseTicket.execute.mockResolvedValue({ ticketId: 'any_ticket_id' })
   })
 
   beforeEach(() => {
@@ -31,8 +40,19 @@ describe('PurchaseTicketController', () => {
     expect(purchaseTicket.execute).toHaveBeenCalledWith({ eventId: 'any_event_id', email: 'any_email', creditCardToken: 'any_credit_card_token' })
   })
 
+  it('should return 500 if purchase ticket throws', async () => {
+    const error = new Error('infra_error')
+    purchaseTicket.execute.mockRejectedValueOnce(error)
+    const httpResponse = await sut.handle({ eventId: 'any_event_id', email: 'any_email', creditCardToken: 'any_credit_card_token' })
+
+    expect(httpResponse).toEqual({
+      statusCode: 500,
+      data: new ServerError(error)
+    })
+  })
+
   it('should return 400 if purchase ticket fails', async () => {
-    purchaseTicket.execute.mockResolvedValueOnce(new EventNotFoundError())
+    purchaseTicket.execute.mockRejectedValueOnce(new EventNotFoundError())
     const httpResponse = await sut.handle({ eventId: 'any_event_id', email: 'any_email', creditCardToken: 'any_credit_card_token' })
 
     expect(httpResponse).toEqual({
