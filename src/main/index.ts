fb5e013ca3e072319f5eacedaf2682f9470ac179
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-18 17:35:40 -0300

    feat: use cors

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,8 @@
 import './config/module-alias'
 
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
-app.use()
+app.use(cors())
 app.listen(8080, () => console.log('Server running at http://localhost:8080'))
