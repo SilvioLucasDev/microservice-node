44a3476d76597308630f5328e7fba049f72b547c
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-18 19:27:00 -0300

    feat: ensure ExpressRouter responds with 200 and valid data

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -3,7 +3,9 @@ import { type Response, type Request } from 'express'
 
 export class ExpressRouter {
   constructor (private readonly controller: Controller) {}
+
   async adapt (req: Request, res: Response): Promise<void> {
-    await this.controller.handle({ ...req.body })
+    const httpResponse = await this.controller.handle({ ...req.body })
+    res.status(200).json(httpResponse.data)
   }
 }
