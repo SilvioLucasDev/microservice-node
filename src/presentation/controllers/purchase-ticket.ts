fd4ddaac3a08233ab34906738ce6eaaf27038f39
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-17 23:04:42 -0300

    feat: ensure PurchaseTicketController return 400 if params is missing

--- src/presentation/controllers/purchase-ticket.ts
+++ src/presentation/controllers/purchase-ticket.ts
@@ -6,6 +6,16 @@ export class PurchaseTicketController {
   ) {}
 
   async handle (httpRequest: any): Promise<HttpResponse> {
+    if (httpRequest.eventId === undefined || httpRequest.eventId === null || httpRequest.eventId === '' ||
+    httpRequest.email === undefined || httpRequest.email === null || httpRequest.email === '' ||
+    httpRequest.creditCardToken === undefined || httpRequest.creditCardToken === null || httpRequest.creditCardToken === ''
+    ) {
+      return {
+        statusCode: 400,
+        data: new Error('The fields in required')
+      }
+    }
+
     const result = await this.purchaseTicket.execute({ eventId: httpRequest.eventId, email: httpRequest.email, creditCardToken: httpRequest.creditCardToken })
     return {
       statusCode: 400,
