c84172c5e6bf43885e8741b56b1a4389411efd44
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-19 16:44:13 -0300

    test: ensure ProcessPaymentUseCase return UnprocessedPaymentError if PaymentGateway returns status diff approved

--- tests/application/use-cases/process-payment.spec.ts
+++ tests/application/use-cases/process-payment.spec.ts
@@ -1,11 +1,14 @@
 import { ProcessPaymentUseCase } from '@/application/use-cases'
 import { type MakePayment } from '@/application/contracts/gateways'
+import { UnprocessedPaymentError } from '@/application/errors/payment'
+import { type UUIDGenerator } from '@/application/contracts/adapters'
 
 import { mock, type MockProxy } from 'jest-mock-extended'
 
 describe('ProcessPaymentUseCase', () => {
   let sut: ProcessPaymentUseCase
   let paymentGateway: MockProxy<MakePayment>
+  let crypto: MockProxy<UUIDGenerator>
   let ticketId: string
   let email: string
   let eventId: string
@@ -14,6 +17,9 @@ describe('ProcessPaymentUseCase', () => {
 
   beforeAll(() => {
     paymentGateway = mock()
+    paymentGateway.makePayment.mockResolvedValue({ tid: '12344', status: 'approved' })
+    crypto = mock()
+    crypto.uuid.mockReturnValue('any_ticket_id')
     ticketId = 'any_ticket_id'
     email = 'any_email'
     eventId = 'any_event_id'
@@ -22,13 +28,21 @@ describe('ProcessPaymentUseCase', () => {
   })
 
   beforeEach(() => {
-    sut = new ProcessPaymentUseCase(paymentGateway)
+    sut = new ProcessPaymentUseCase(paymentGateway, crypto)
   })
 
-  it('should call method makePayment of PaymentGateway with correct value', async () => {
+  it('should call method makePayment of PaymentGateway with correct values', async () => {
     await sut.execute({ ticketId, email, eventId, price, creditCardToken })
 
     expect(paymentGateway.makePayment).toHaveBeenCalledWith({ email, creditCardToken, price })
     expect(paymentGateway.makePayment).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw UnprocessedPaymentError if PaymentGateway returns status diff approved', async () => {
+    paymentGateway.makePayment.mockResolvedValueOnce({ tid: '12344', status: 'rejected' })
+
+    const promise = sut.execute({ ticketId, email, eventId, price, creditCardToken })
+
+    await expect(promise).rejects.toThrow(new UnprocessedPaymentError())
   })
 })
