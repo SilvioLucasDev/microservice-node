6fdd3e8d226b6980b2e7cb4ee3f28b2ef9099bb4
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-19 16:46:57 -0300

    feat: ensure ProcessPaymentUseCase calls Transactions with correct values

--- src/application/use-cases/process-payment.ts
+++ src/application/use-cases/process-payment.ts
@@ -1,12 +1,18 @@
 import { type MakePayment } from '@/application/contracts/gateways'
 import { UnprocessedPaymentError } from '@/application/errors/payment'
+import { type UUIDGenerator } from '@/application/contracts/adapters'
+import { Transaction } from '@/domain/entities'
 
 export class ProcessPaymentUseCase {
-  constructor (private readonly paymentGateway: MakePayment) {}
+  constructor (
+    private readonly paymentGateway: MakePayment,
+    private readonly crypto: UUIDGenerator
+  ) {}
 
   async execute ({ ticketId, email, eventId, price, creditCardToken }: Input): Promise<void> {
     const payment = await this.paymentGateway.makePayment({ email, creditCardToken, price })
     if (payment.status !== 'approved') throw new UnprocessedPaymentError()
+    Transaction.create({ eventId, ticketId, price }, this.crypto)
   }
 }
 
