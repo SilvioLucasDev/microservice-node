dd8adf0463f64fc7832cf38ded5f8e3644b57347
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-18 22:09:01 -0300

    fix: adjust parameters passed to prism

--- src/infra/repositories/postgres/ticket.ts
+++ src/infra/repositories/postgres/ticket.ts
@@ -2,9 +2,9 @@ import { type SaveTicket } from '@/domain/contracts/repos'
 import prisma from '@/infra/repositories/postgres/helpers/connection'
 
 export class PgTicketRepository implements SaveTicket {
-  async save (input: SaveTicket.Input): Promise<void> {
+  async save ({ id, email, eventId, status }: SaveTicket.Input): Promise<void> {
     await prisma.ticket.create({
-      data: { ...input, event_id: input.eventId }
+      data: { id, email, event_id: eventId, status }
     })
   }
 }
