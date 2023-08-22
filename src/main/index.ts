e18522084bca0308e08f66de2fc3c69d04bd934f
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-22 12:56:58 -0300

    feat: add start queue in index

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,7 @@
 import './config/module-alias'
 import { SetupExpress } from '@/main/config/express'
 
+import { makeQueueController as initQueue } from '@/main/factories/infra/adapters/queue'
+
 new SetupExpress().init()
+initQueue()
