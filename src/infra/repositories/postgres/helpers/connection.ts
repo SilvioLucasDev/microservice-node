757172abdaa8c14b39482101812a2be2834b97fa
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-21 23:47:13 -0300

    refactor: remove code unnecessary

--- src/infra/repositories/postgres/helpers/connection.ts
+++ src/infra/repositories/postgres/helpers/connection.ts
@@ -3,10 +3,6 @@ import { PrismaClient } from '@prisma/client'
 class PrismaConnection {
   private static instance?: PrismaClient
 
-  private constructor () {
-    PrismaConnection.instance = new PrismaClient()
-  }
-
   public static getInstance (): PrismaClient {
     if (PrismaConnection.instance == null) PrismaConnection.instance = new PrismaClient()
     return PrismaConnection.instance
