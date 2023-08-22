0a42b54a7c2d7f8218af8f07212e5d8c1b5adcf1
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-22 16:35:46 -0300

    test:  add new ExpressAdapter tests

---
+++ tests/presentation/adapters/express.spec.ts
@@ -0,0 +1,90 @@
+import { env } from '@/main/config/env'
+import { ExpressAdapter } from '@/presentation/adapters'
+import request from 'supertest'
+
+describe('ExpressRouterAdapter', () => {
+  let sut: ExpressAdapter
+  let method: string
+  let url: string
+
+  beforeAll(() => {
+    method = 'post'
+    url = '/ticket/purchase'
+  })
+
+  beforeEach(() => {
+    sut = new ExpressAdapter()
+  })
+
+  it('should responde with 200 and valid data', async () => {
+    sut.on({
+      method,
+      url,
+      callback: async (params: any, body: any) => ({
+        statusCode: 200,
+        data: { data: 'any_data' }
+      })
+    })
+
+    const response = await request(sut.app).post('/v1/api/ticket/purchase').send()
+
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual({ data: 'any_data' })
+  })
+
+  it('should respond with 204 and empty data', async () => {
+    sut.on({
+      method,
+      url,
+      callback: async (params: any, body: any) => ({
+        statusCode: 204,
+        data: null
+      })
+    })
+
+    const response = await request(sut.app).post('/v1/api/ticket/purchase').send()
+
+    expect(response.status).toBe(204)
+    expect(response.body).toEqual({ })
+  })
+
+  it('should responde with 400 and valid error', async () => {
+    sut.on({
+      method,
+      url,
+      callback: async (params: any, body: any) => ({
+        statusCode: 400,
+        data: new Error('any_error')
+      })
+    })
+
+    const response = await request(sut.app).post('/v1/api/ticket/purchase').send()
+
+    expect(response.status).toBe(400)
+    expect(response.body).toEqual({ error: 'any_error' })
+  })
+
+  it('should respond with 500 and valid error', async () => {
+    sut.on({
+      method,
+      url,
+      callback: async (params: any, body: any) => ({
+        statusCode: 500,
+        data: new Error('any_error')
+      })
+    })
+
+    const response = await request(sut.app).post('/v1/api/ticket/purchase').send()
+
+    expect(response.status).toBe(500)
+    expect(response.body).toEqual({ error: 'any_error' })
+  })
+
+  it('should call method listen with port correct', () => {
+    sut.app.listen = jest.fn()
+
+    sut.listen()
+
+    expect(sut.app.listen).toHaveBeenCalledWith(env.port, expect.any(Function))
+  })
+})
