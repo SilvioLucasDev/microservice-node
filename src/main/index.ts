91fa352bde6489670051416c7644e4e5c3dbff6e
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-22 15:27:59 -0300

    refactor:  refactor: move route factory file to main folder

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,6 @@
 import './config/module-alias'
 import { makeQueueController as initQueue } from '@/main/factories/infra/adapters/queue'
-import { makeHttpServer as initHttpServer } from './factories/routes'
+import { makeHttpServer as initHttpServer } from '@/main/factories/main/routes'
 
 initHttpServer()
 initQueue()
