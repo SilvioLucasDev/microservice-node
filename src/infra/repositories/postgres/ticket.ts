872288a23eb25f515fbfb5204d6ecc9297bf1fb0
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-26 00:39:05 -0300

    feat: ensure return undefined an update ticket

--- src/infra/repositories/postgres/ticket.ts
+++ src/infra/repositories/postgres/ticket.ts
@@ -1,10 +1,16 @@
-import { type SaveTicket } from '@/application/contracts/repositories'
+import { type UpdateStatusTicket, type SaveTicket } from '@/application/contracts/repositories'
 import prisma from '@/infra/repositories/postgres/helpers/connection'
 
-export class PgTicketRepository implements SaveTicket {
+export class PgTicketRepository implements SaveTicket, UpdateStatusTicket {
   async save ({ id, email, eventId, status }: SaveTicket.Input): Promise<void> {
     await prisma.ticket.create({
       data: { id, email, event_id: eventId, status }
+    })
+  }
+
+  async updateStatus ({ id, status }: UpdateStatusTicket.Input): Promise<any> {
+    await prisma.ticket.update({
+      where: { id }, data: { status }
     })
   }
 }
