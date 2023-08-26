7e315d0b2506a6247419f1f3836c8b38a3273194
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-26 00:43:46 -0300

    feat: should QueueController calls ProcessTicketUseCase

--- src/infra/adapters/queue/controller.ts
+++ src/infra/adapters/queue/controller.ts
@@ -1,5 +1,6 @@
 import { type Consume } from '@/application/contracts/adapters'
 import { makeProcessPaymentUseCase } from '@/main/factories/application/use-cases'
+import { makeProcessTicketUseCase } from '@/main/factories/application/use-cases/process-ticket'
 
 export class QueueController {
   constructor (private readonly queue: Consume) {
@@ -7,6 +8,13 @@ export class QueueController {
       queueName: 'ticketReserved',
       callback: async (input: any) => {
         await makeProcessPaymentUseCase().execute(input)
+      }
+    })
+
+    this.queue.consume({
+      queueName: 'paymentProcessed',
+      callback: async (input: any) => {
+        await makeProcessTicketUseCase().execute(input)
       }
     })
   }
