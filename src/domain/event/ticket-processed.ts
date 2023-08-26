6b2a22fe67bf752655497da46121031901859fe6
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-26 20:22:14 -0300

    refactor: rename attribute

--- src/domain/event/ticket-processed.ts
+++ src/domain/event/ticket-processed.ts
@@ -1,6 +1,6 @@
 export class TicketProcessed {
   constructor (
-    private readonly from: string,
+    private readonly to: string,
     private readonly subject: string,
     private readonly email: string,
     private readonly body: string
