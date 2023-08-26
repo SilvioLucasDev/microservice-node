44812c2e9a90d53e7300c77a1e6ed7dd841673db
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-26 19:11:03 -0300

    feat: ensure PgTicketRepository returns event email and name

--- src/infra/repositories/postgres/ticket.ts
+++ src/infra/repositories/postgres/ticket.ts
@@ -1,7 +1,7 @@
-import { type UpdateStatusTicket, type SaveTicket } from '@/application/contracts/repositories'
+import { type UpdateStatusTicket, type SaveTicket, type FindDetailsByIdTicket } from '@/application/contracts/repositories'
 import prisma from '@/infra/repositories/postgres/helpers/connection'
 
-export class PgTicketRepository implements SaveTicket, UpdateStatusTicket {
+export class PgTicketRepository implements SaveTicket, UpdateStatusTicket, FindDetailsByIdTicket {
   async save ({ id, email, eventId, status }: SaveTicket.Input): Promise<void> {
     await prisma.ticket.create({
       data: { id, email, event_id: eventId, status }
@@ -12,5 +12,24 @@ export class PgTicketRepository implements SaveTicket, UpdateStatusTicket {
     await prisma.ticket.update({
       where: { id }, data: { status }
     })
+  }
+
+  async findDetailsById ({ id }: FindDetailsByIdTicket.Input): Promise<FindDetailsByIdTicket.Output> {
+    const ticket = await prisma.ticket.findFirst({
+      where: { id },
+      select: {
+        email: true,
+        event: {
+          select: { name: true }
+        }
+      }
+    })
+
+    console.log(ticket)
+
+    return {
+      email: ticket!.email,
+      eventName: ticket!.event.name
+    }
   }
 }
