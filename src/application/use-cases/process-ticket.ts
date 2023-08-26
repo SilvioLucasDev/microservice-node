24f087b7b171434d249a1a89f0cefbd28b9787b6
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-26 20:24:02 -0300

    feat: ensure ProcessTicketUseCase call TicketReserved with correct values

--- src/application/use-cases/process-ticket.ts
+++ src/application/use-cases/process-ticket.ts
@@ -1,5 +1,7 @@
 import { type FindDetailsByIdTicket, type UpdateStatusTicket } from '@/application/contracts/repositories'
 import { Email, Ticket } from '@/domain/entities'
+import { TicketProcessed } from '@/domain/event'
+import { env } from '@/main/config/env'
 
 export class ProcessTicketUseCase {
   constructor (
@@ -10,7 +12,8 @@ export class ProcessTicketUseCase {
     const ticketStatus = status === 'approved' ? Ticket.approve() : Ticket.cancel()
     await this.ticketRepository.updateStatus({ id: ticketId, status: ticketStatus })
     const { email, eventName } = await this.ticketRepository.findDetailsById({ id: ticketId })
-    Email.create({ ticketId, email, eventName, ticketStatus })
+    const emailData = Email.create({ ticketId, email, eventName, ticketStatus })
+    new TicketProcessed(env.emailService, emailData.subject, emailData.email, emailData.body)
   }
 }
 
