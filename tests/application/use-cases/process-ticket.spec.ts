5a492709b93e6562c31d5fe7e754ed54d5c00f1d
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-26 19:49:12 -0300

    test: ensure ProcessTicketUseCase call EmailEntity with correct values

--- tests/application/use-cases/process-ticket.spec.ts
+++ tests/application/use-cases/process-ticket.spec.ts
@@ -1,18 +1,24 @@
 import { ProcessTicketUseCase } from '@/application/use-cases'
 import { type FindDetailsByIdTicket, type UpdateStatusTicket } from '@/application/contracts/repositories'
+import { Email } from '@/domain/entities'
 
 import { mock, type MockProxy } from 'jest-mock-extended'
 
 describe('ProcessTicketUseCase', () => {
   let sut: ProcessTicketUseCase
   let ticketRepository: MockProxy<UpdateStatusTicket & FindDetailsByIdTicket>
+  let emailEntity: jest.SpyInstance
   let ticketId: string
   let email: string
+  let status: string
 
   beforeAll(() => {
     ticketRepository = mock()
+    ticketRepository.findDetailsById.mockResolvedValue({ email: 'any_email', eventName: 'any_event_name' })
+    emailEntity = jest.spyOn(Email, 'create')
     ticketId = 'any_ticket_id'
     email = 'any_email'
+    status = 'approved'
   })
 
   beforeEach(() => {
@@ -20,23 +26,30 @@ describe('ProcessTicketUseCase', () => {
   })
 
   it('should calls method updateStatus of TicketRepository with correct value if payment is approved', async () => {
-    await sut.execute({ ticketId, status: 'approved', email })
+    await sut.execute({ ticketId, status })
 
-    expect(ticketRepository.updateStatus).toHaveBeenCalledWith({ id: ticketId, status: 'approved' })
+    expect(ticketRepository.updateStatus).toHaveBeenCalledWith({ id: ticketId, status })
     expect(ticketRepository.updateStatus).toHaveBeenCalledTimes(1)
   })
 
   it('should calls method updateStatus of TicketRepository with correct value if payment is approved', async () => {
-    await sut.execute({ ticketId, status: 'rejected', email })
+    await sut.execute({ ticketId, status: 'rejected' })
 
     expect(ticketRepository.updateStatus).toHaveBeenCalledWith({ id: ticketId, status: 'cancelled' })
     expect(ticketRepository.updateStatus).toHaveBeenCalledTimes(1)
   })
 
   it('should calls method findDetailsById of TicketRepository with correct value', async () => {
-    await sut.execute({ ticketId, status: 'approved', email })
+    await sut.execute({ ticketId, status })
 
     expect(ticketRepository.findDetailsById).toHaveBeenCalledWith({ id: ticketId })
     expect(ticketRepository.findDetailsById).toHaveBeenCalledTimes(1)
+  })
+
+  it('should calls EmailEntity with correct values', async () => {
+    await sut.execute({ ticketId, status })
+
+    expect(emailEntity).toHaveBeenCalledWith({ ticketId, email, eventName: 'any_event_name', ticketStatus: 'approved' })
+    expect(emailEntity).toHaveBeenCalledTimes(1)
   })
 })
