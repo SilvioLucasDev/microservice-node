61cbb5ac96df6d51af30797caa9cffce21fe6f76
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-26 19:14:30 -0300

    feat: ensure ProcessTicketUseCase call method findDetailsById of TicketRepository with correct value

--- src/application/use-cases/process-ticket.ts
+++ src/application/use-cases/process-ticket.ts
@@ -1,14 +1,15 @@
-import { type UpdateStatusTicket } from '@/application/contracts/repositories'
+import { type FindDetailsByIdTicket, type UpdateStatusTicket } from '@/application/contracts/repositories'
 import { Ticket } from '@/domain/entities'
 
 export class ProcessTicketUseCase {
   constructor (
-    private readonly ticketRepository: UpdateStatusTicket
+    private readonly ticketRepository: UpdateStatusTicket & FindDetailsByIdTicket
   ) {}
 
   async execute ({ ticketId, status }: Input): Promise<void> {
     const statusTicket = status === 'approved' ? Ticket.approve() : Ticket.cancel()
     await this.ticketRepository.updateStatus({ id: ticketId, status: statusTicket })
+    this.ticketRepository.findDetailsById({ id: ticketId })
     console.log('Email enviado para ??')
   }
 }
