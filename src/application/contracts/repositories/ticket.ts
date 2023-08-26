9599ed55b8d3f6c05c0cd53378dea19a380437a6
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-25 23:13:07 -0300

    feat: add method findById in interface of TicketRepository

--- src/application/contracts/repositories/ticket.ts
+++ src/application/contracts/repositories/ticket.ts
@@ -7,3 +7,15 @@ export interface SaveTicket {
 export namespace SaveTicket {
   export type Input = Ticket
 }
+
+export interface FindByIdTicket {
+  findById: (id: FindByIdTicket.Input) => Promise<void>
+}
+
+export namespace FindByIdTicket {
+  export type Input = {
+    id: string
+  }
+
+  export type Output = Ticket
+}
