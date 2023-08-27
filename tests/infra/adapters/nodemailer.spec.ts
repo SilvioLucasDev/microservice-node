5067b8f64d8aae46a4340f6a185e562f2a825358
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-26 23:24:38 -0300

    test: ensure NodeMailerAdapter calls method sendMail with correct values

---
+++ tests/infra/adapters/nodemailer.spec.ts
@@ -0,0 +1,38 @@
+import { NodeMailerAdapter } from '@/infra/adapters'
+
+import nodemailer from 'nodemailer'
+
+jest.mock('nodemailer')
+
+describe('NodeMailerAdapter', () => {
+  let sut: NodeMailerAdapter
+  let from: string
+  let to: string
+  let subject: string
+  let body: string
+  let sendMailMock: jest.Mock
+
+  beforeAll(() => {
+    from = 'any_from'
+    to = 'any_to'
+    subject = 'any_subject'
+    body = 'any_body'
+
+    sendMailMock = jest.fn()
+    jest.mocked(nodemailer.createTransport).mockReturnValue({
+      sendMail: sendMailMock
+    } as any)
+  })
+
+  beforeEach(() => {
+    sut = new NodeMailerAdapter()
+  })
+
+  it('should calls method sendMail with correct values', async () => {
+    await sut.send({ from, to, subject, body })
+
+    expect(sendMailMock).toHaveBeenCalledWith({
+      from, to, subject, html: body
+    })
+  })
+})
