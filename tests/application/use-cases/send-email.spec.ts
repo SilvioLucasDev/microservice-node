671431f6fb017b2a8e300f6a669d7a927c690ad5
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-08-26 22:35:26 -0300

    test: ensure SendEmailUseCase call method send of EmailAdapter with correct values

---
+++ tests/application/use-cases/send-email.spec.ts
@@ -0,0 +1,33 @@
+import { SendEmailUseCase } from '@/application/use-cases'
+import { type Send } from '@/application/contracts/adapters'
+
+import { mock, type MockProxy } from 'jest-mock-extended'
+
+describe('SendEmailUseCase', () => {
+  let sut: SendEmailUseCase
+  let emailAdapter: MockProxy<Send>
+
+  let from: string
+  let to: string
+  let subject: string
+  let body: string
+
+  beforeAll(() => {
+    emailAdapter = mock()
+    from = 'any_from'
+    to = 'any_to'
+    subject = 'any_subject'
+    body = 'any_body'
+  })
+
+  beforeEach(() => {
+    sut = new SendEmailUseCase(emailAdapter)
+  })
+
+  it('should call method send of EmailAdapter with correct values', async () => {
+    await sut.execute({ from, to, subject, body })
+
+    expect(emailAdapter.send).toHaveBeenCalledWith({ from, to, subject, body })
+    expect(emailAdapter.send).toHaveBeenCalledTimes(1)
+  })
+})
