dc9a98b96b1b579d3137b02726116bbcdefda31b
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-12 14:34:30 -0300

    feat: ensure Ticket return statuses correctly

--- src/domain/entities/ticket.ts
+++ src/domain/entities/ticket.ts
@@ -1,29 +1,37 @@
 import { type UUIDGenerator } from '@/application/contracts/adapters'
+import { TicketStatus } from '@/domain/enums'
 
 export class Ticket {
   constructor (
     readonly id: string,
     readonly eventId: string,
-    readonly email: string,
+    readonly userId: string,
     public status: string
-  ) {}
+  ) { }
 
-  static create ({ eventId, email }: Input, crypto: UUIDGenerator): Ticket {
+  static create ({ eventId, userId }: Input, crypto: UUIDGenerator): Ticket {
     const id = crypto.uuid()
-    const initialStatus = 'reserved'
-    return new Ticket(id, eventId, email, initialStatus)
+    const initialStatus = TicketStatus.RESERVED
+    return new Ticket(id, eventId, userId, initialStatus)
   }
 
-  static approve (): string {
-    return 'approved'
-  }
-
-  static cancel (): string {
-    return 'cancelled'
+  static statusMap (status: string): string {
+    const statusMap: Record<string, string> = {
+      started: TicketStatus.RESERVED,
+      processing: TicketStatus.RESERVED,
+      pending: TicketStatus.RESERVED,
+      approved: TicketStatus.APPROVED,
+      refused: TicketStatus.CANCELLED,
+      refunded: TicketStatus.CANCELLED,
+      chargeback: TicketStatus.CANCELLED,
+      error: TicketStatus.CANCELLED
+    }
+    // TODO: Se o status que for recebido não ter mapeamento deve estourar uma exception e gerar um log
+    return statusMap[status]
   }
 }
 
 type Input = {
   eventId: string
-  email: string
+  userId: string
 }
