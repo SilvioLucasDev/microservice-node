3d84a6571b7501c78906077a79a2a66aa8e0bac6
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-12 15:57:12 -0300

    test: ensure PgCardRepository return card if exists

---
+++ tests/infra/repositories/postgres/card.spec.ts
@@ -0,0 +1,32 @@
+import { prismaMock } from '@/tests/infra/repositories/postgres/mocks'
+import { PgCardRepository } from '@/infra/repositories/postgres'
+
+import { type Prisma, type Card as CardPrisma } from '@prisma/client'
+
+describe('PgCardRepository', () => {
+  let id: string
+  let alias: string
+  let token: string
+
+  let sut: PgCardRepository
+
+  beforeEach(() => {
+    id = 'any_id'
+    alias = 'any_alias'
+    token = 'any_token'
+
+    sut = new PgCardRepository()
+  })
+
+  afterAll(async () => {
+    await prismaMock.$disconnect()
+  })
+
+  it('should return card if exists', async () => {
+    prismaMock.card.findFirst.mockResolvedValueOnce({ id, alias, token } as unknown as Prisma.Prisma__CardClient<CardPrisma>)
+
+    const card = await sut.get({ id })
+
+    expect(card).toEqual({ id, alias, token })
+  })
+})
