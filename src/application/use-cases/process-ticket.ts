53273a36ee120dd6936a3cfdf76302be21b03634
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-12 13:26:39 -0300

    refactor: ensure ProcessTicketUseCase calls UserRepository, TicketRepository and TicketEntity

--- src/application/use-cases/process-ticket.ts
+++ src/application/use-cases/process-ticket.ts
@@ -1,4 +1,4 @@
-import { type FindDetailsByIdTicket, type UpdateStatusTicket } from '@/application/contracts/repositories'
+import { type GetUser, type FindDetailsByIdTicket, type UpdateStatusTicket } from '@/application/contracts/repositories'
 import { Email, Ticket } from '@/domain/entities'
 import { TicketProcessed } from '@/domain/event'
 import { env } from '@/main/config/env'
@@ -7,14 +7,16 @@ import { type Publish } from '@/application/contracts/adapters'
 export class ProcessTicketUseCase {
   constructor (
     private readonly ticketRepository: UpdateStatusTicket & FindDetailsByIdTicket,
+    private readonly userRepository: GetUser,
     private readonly queue: Publish
-  ) {}
+  ) { }
 
-  async execute ({ ticketId, status }: Input): Promise<void> {
-    const ticketStatus = status === 'approved' ? Ticket.approve() : Ticket.cancel()
+  async execute ({ ticketId, paymentType, url, userId, status }: Input): Promise<void> {
+    const ticketStatus = Ticket.statusMap(status)
     await this.ticketRepository.updateStatus({ id: ticketId, status: ticketStatus })
-    const { email, eventName } = await this.ticketRepository.findDetailsById({ id: ticketId })
-    const emailData = Email.create({ ticketId, email, eventName, ticketStatus })
+    const { eventName } = await this.ticketRepository.findDetailsById({ id: ticketId })
+    const user = await this.userRepository.get({ id: userId }) // TODO Isso deve ser uma requisição HTTP para o service de usuários.
+    const emailData = Email.create({ ticketId, ticketStatus, paymentType, url, email: user!.email, eventName })
     const ticketProcessed = new TicketProcessed(env.emailSender, emailData.email, emailData.subject, emailData.body)
     await this.queue.publish({ queueName: 'ticketProcessed', data: ticketProcessed })
   }
@@ -22,5 +24,8 @@ export class ProcessTicketUseCase {
 
 type Input = {
   ticketId: string
+  paymentType: string
+  url: string
+  userId: string
   status: string
 }
--- tests/application/use-cases/process-ticket.spec.ts
+++ tests/application/use-cases/process-ticket.spec.ts
@@ -1,8 +1,8 @@
 import { ProcessTicketUseCase } from '@/application/use-cases'
 import { type Publish } from '@/application/contracts/adapters'
-import { Email } from '@/domain/entities'
+import { Email, Ticket } from '@/domain/entities'
 import { TicketProcessed } from '@/domain/event'
-import { type FindDetailsByIdTicket, type UpdateStatusTicket } from '@/application/contracts/repositories'
+import { type GetUser, type FindDetailsByIdTicket, type UpdateStatusTicket } from '@/application/contracts/repositories'
 import { env } from '@/main/config/env'
 
 import { mock, type MockProxy } from 'jest-mock-extended'
@@ -11,73 +11,105 @@ jest.mock('@/domain/event/ticket-processed')
 
 describe('ProcessTicketUseCase', () => {
   let ticketId: string
-  let email: string
+  let paymentType: string
+  let url: string
+  let userId: string
   let status: string
   let eventName: string
+  let id: string
+  let name: string
+  let document: string
+  let email: string
+  let zipcode: string
+  let number: string
+  let complements: string
+  let mobilePhone: string
 
   let sut: ProcessTicketUseCase
   let ticketRepository: MockProxy<UpdateStatusTicket & FindDetailsByIdTicket>
+  let userRepository: MockProxy<GetUser>
   let emailEntity: jest.SpyInstance
+  let ticketEntity: jest.SpyInstance
   let queue: MockProxy<Publish>
 
   beforeAll(() => {
     ticketId = 'any_ticket_id'
-    email = 'any_email'
+    paymentType = 'any_payment_type'
+    url = 'any_url'
+    userId = 'any_user_id'
     status = 'approved'
     eventName = 'any_event_name'
+    id = 'any_id'
+    name = 'any_name'
+    document = 'any_document'
+    email = 'any_email'
+    zipcode = 'any_zipcode'
+    number = 'any_number'
+    complements = 'any_complements'
+    mobilePhone = 'any_mobile_phone'
 
     ticketRepository = mock()
-    ticketRepository.findDetailsById.mockResolvedValue({ email, eventName })
+    ticketRepository.findDetailsById.mockResolvedValue({ eventName })
+    userRepository = mock()
+    userRepository.get.mockResolvedValue({ id, name, document, email, zipcode, number, complements, mobile_phone: mobilePhone })
     emailEntity = jest.spyOn(Email, 'create')
+    ticketEntity = jest.spyOn(Ticket, 'statusMap')
     queue = mock()
   })
 
   beforeEach(() => {
-    sut = new ProcessTicketUseCase(ticketRepository, queue)
+    sut = new ProcessTicketUseCase(ticketRepository, userRepository, queue)
   })
 
-  it('should call method updateStatus of TicketRepository with correct value if payment is approved', async () => {
-    await sut.execute({ ticketId, status })
+  it('should call method statusMap of TicketEntity with correct value', async () => {
+    await sut.execute({ ticketId, paymentType, url, userId, status })
+
+    expect(ticketEntity).toHaveBeenCalledWith(status)
+    expect(ticketEntity).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call method updateStatus of TicketRepository with correct values', async () => {
+    await sut.execute({ ticketId, paymentType, url, userId, status })
 
     expect(ticketRepository.updateStatus).toHaveBeenCalledWith({ id: ticketId, status })
     expect(ticketRepository.updateStatus).toHaveBeenCalledTimes(1)
   })
 
-  it('should call method updateStatus of TicketRepository with correct value if payment is rejected', async () => {
-    await sut.execute({ ticketId, status: 'rejected' })
-
-    expect(ticketRepository.updateStatus).toHaveBeenCalledWith({ id: ticketId, status: 'cancelled' })
-    expect(ticketRepository.updateStatus).toHaveBeenCalledTimes(1)
-  })
-
   it('should call method findDetailsById of TicketRepository with correct value', async () => {
-    await sut.execute({ ticketId, status })
+    await sut.execute({ ticketId, paymentType, url, userId, status })
 
     expect(ticketRepository.findDetailsById).toHaveBeenCalledWith({ id: ticketId })
     expect(ticketRepository.findDetailsById).toHaveBeenCalledTimes(1)
   })
 
-  it('should call EmailEntity with correct values', async () => {
-    await sut.execute({ ticketId, status })
+  it('should call method get of UserRepository with correct value', async () => {
+    await sut.execute({ ticketId, paymentType, url, userId, status })
 
-    expect(emailEntity).toHaveBeenCalledWith({ ticketId, email, eventName, ticketStatus: 'approved' })
+    expect(userRepository.get).toHaveBeenCalledWith({ id: userId })
+    expect(userRepository.get).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call EmailEntity with correct values', async () => {
+    await sut.execute({ ticketId, paymentType, url, userId, status })
+
+    expect(emailEntity).toHaveBeenCalledWith({ ticketId, ticketStatus: 'approved', paymentType, url, email, eventName })
     expect(emailEntity).toHaveBeenCalledTimes(1)
   })
 
-  it('should call TicketReserved with correct values', async () => {
-    await sut.execute({ ticketId, status })
+  it('should call TicketProcessed with correct values', async () => {
+    await sut.execute({ ticketId, paymentType, url, userId, status })
 
     expect(TicketProcessed).toHaveBeenCalledWith(
       env.emailSender,
       'any_email',
       'Ticket Purchase | any_event_name',
-      'Hello! <br><br> Ticket payment: any_ticket_id for the any_event_name event was successfully completed!'
+      expect.stringMatching(/Hello! <br><br> Ticket payment: any_ticket_id for event any_event_name was successful!.*/)
     )
     expect(TicketProcessed).toHaveBeenCalledTimes(1)
   })
 
   it('should call QueueAdapter with correct values', async () => {
-    await sut.execute({ ticketId, status })
+    await sut.execute({ ticketId, paymentType, url, userId, status })
 
     expect(queue.publish).toHaveBeenCalledWith({ queueName: 'ticketProcessed', data: expect.any(TicketProcessed) })
     expect(queue.publish).toHaveBeenCalledTimes(1)
