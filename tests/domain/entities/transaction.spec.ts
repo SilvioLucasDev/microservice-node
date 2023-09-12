a12b3a947db8048e022dbe52e33038c2233dc3f4
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-12 15:30:47 -0300

    test: ensure TransactionEntity update the Transaction instance with new values

--- tests/domain/entities/transaction.spec.ts
+++ tests/domain/entities/transaction.spec.ts
@@ -1,36 +1,78 @@
 import { type UUIDGenerator } from '@/application/contracts/adapters'
 import { Transaction } from '@/domain/entities'
+import { PaymentTypeInvalidError, TransactionStatusInvalidError } from '@/domain/errors'
 
 import { mock, type MockProxy } from 'jest-mock-extended'
+import MockDate from 'mockdate'
 
 describe('TransactionEntity', () => {
-  let eventId: string
   let ticketId: string
-  let tid: string
-  let price: string
-  let status: string
+  let paymentType: string
+  let cardId: string
+  let total: number
+  let installments: number
+  let dueDate: Date
   let transactionId: string
+  let initialStatus: string
+  let processorResponse: string
+  let status: string
 
   let sut: Transaction
   let crypto: MockProxy<UUIDGenerator>
 
+  function getDueDate (): Date {
+    const dueDate = new Date()
+    dueDate.setDate(dueDate.getDate() + 10)
+    return dueDate
+  }
+
   beforeAll(() => {
-    eventId = 'any_event_id'
+    MockDate.set(new Date())
+
     ticketId = 'any_ticket_id'
-    tid = 'any_tid'
-    price = 'any_price'
-    status = 'any_status'
+    paymentType = 'credit_card'
+    cardId = 'any_card_id'
+    total = 300
+    installments = 3
+    dueDate = getDueDate()
     transactionId = 'any_transaction_id'
+    initialStatus = 'started'
+    processorResponse = 'any_processor_response'
+    status = 'approved'
 
     crypto = mock()
     crypto.uuid.mockReturnValue(transactionId)
   })
 
-  beforeEach(() => {
-    sut = Transaction.create({ eventId, ticketId, tid, price, status }, crypto)
+  afterAll(() => {
+    MockDate.reset()
   })
 
-  it('should return instance of TransactionEntity with correct values', () => {
-    expect(sut).toStrictEqual(new Transaction(transactionId, eventId, ticketId, tid, price, status))
+  describe('create', () => {
+    it('should create a Transaction instance with correct values', () => {
+      sut = Transaction.create({ ticketId, paymentType, cardId, total, installments }, crypto)
+
+      expect(sut).toStrictEqual(new Transaction(transactionId, ticketId, paymentType, cardId, total, installments, dueDate, '', '', initialStatus))
+    })
+
+    it('should throw PaymentTypeInvalidError for invalid paymentType', () => {
+      expect(() => Transaction.create({ ticketId, paymentType: 'invalid_payment_type', cardId, total, installments }, crypto))
+        .toThrow(PaymentTypeInvalidError)
+    })
+  })
+
+  describe('update', () => {
+    it('should update the Transaction instance with new values', () => {
+      sut.update({ processorResponse, transactionId, status })
+
+      expect(sut.processorResponse).toBe(processorResponse)
+      expect(sut.transactionId).toBe(transactionId)
+      expect(sut.status).toBe(status)
+    })
+
+    it('should throw TransactionStatusInvalidError for invalid status', () => {
+      expect(() => sut.update({ processorResponse, transactionId, status: 'invalid_status' }))
+        .toThrow(TransactionStatusInvalidError)
+    })
   })
 })
