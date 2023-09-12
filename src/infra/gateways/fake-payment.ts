b2fbae6c1b03ffbedaab47f1829dac51bcdb553a
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-12 16:23:03 -0300

    refactor: ensure FakePaymentGateway return tid and status when payment is processed

--- src/infra/gateways/fake-payment.ts
+++ src/infra/gateways/fake-payment.ts
@@ -2,10 +2,10 @@ import { type MakePayment } from '@/application/contracts/gateways'
 import { type UUIDGenerator } from '@/application/contracts/adapters'
 
 export class FakePaymentGateway implements MakePayment {
-  constructor (private readonly crypto: UUIDGenerator) {}
+  constructor (private readonly crypto: UUIDGenerator) { }
 
   async makePayment (input: MakePayment.Input): Promise<MakePayment.Output> {
-    const tid = this.crypto.uuid()
-    return { tid, status: 'approved' }
+    const transactionId = this.crypto.uuid()
+    return { transactionId, status: 'approved', url: 'any_url', processorResponse: 'Retorno do gateway' }
   }
 }
--- tests/infra/gateways/fake-payment.spec.ts
+++ tests/infra/gateways/fake-payment.spec.ts
@@ -1,23 +1,36 @@
 import { type UUIDGenerator } from '@/application/contracts/adapters'
 import { FakePaymentGateway } from '@/infra/gateways'
+
 import { mock, type MockProxy } from 'jest-mock-extended'
 
 describe('ProcessPaymentUseCase', () => {
-  let email: string
-  let creditCardToken: string
-  let price: string
+  let transactionId: string
+  let user: object
+  let card: object
+  let total: number
+  let paymentType: string
+  let installments: number
+  let dueDate: Date
   let tid: string
   let status: string
+  let url: string
+  let processorResponse: string
 
   let sut: FakePaymentGateway
   let crypto: MockProxy<UUIDGenerator>
 
   beforeAll(() => {
-    email = 'any_email'
-    creditCardToken = 'any_credit_card_token'
-    price = 'any_price'
+    transactionId = 'any_transaction_id'
+    user = { any: 'any' }
+    card = { any: 'any' }
+    total = 300
+    paymentType = 'credit_card'
+    installments = 3
+    dueDate = new Date()
     tid = 'any_tid'
     status = 'approved'
+    url = 'any_url'
+    processorResponse = 'Retorno do gateway'
 
     crypto = mock()
     crypto.uuid.mockReturnValue(tid)
@@ -28,9 +41,11 @@ describe('ProcessPaymentUseCase', () => {
   })
 
   it('should return tid and status when payment is processed', async () => {
-    const result = await sut.makePayment({ email, price, creditCardToken })
+    const result = await sut.makePayment({ transactionId, user, card, total, paymentType, installments, dueDate })
 
-    expect(result.tid).toBe(tid)
+    expect(result.transactionId).toBe(tid)
     expect(result.status).toBe(status)
+    expect(result.url).toBe(url)
+    expect(result.processorResponse).toBe(processorResponse)
   })
 })
