86d2fa67984e504f5cc3e2f3d77a6aee5a51c92c
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-12 16:16:08 -0300

    feat: ensure PgUserRepository return user if exists

---
+++ src/infra/repositories/postgres/user.ts
@@ -0,0 +1,38 @@
+import { type GetUser } from '@/application/contracts/repositories'
+import prisma from '@/infra/repositories/postgres/helpers/connection'
+
+export class PgUserRepository implements GetUser {
+  async get ({ id }: GetUser.Input): Promise<GetUser.Output> {
+    const user = await prisma.user.findFirst({
+      where: {
+        id
+      },
+      select: {
+        id: true,
+        name: true,
+        document: true,
+        email: true,
+        mobile_phone: true,
+        addresses: {
+          select: {
+            zipcode: true,
+            number: true,
+            complements: true
+          }
+        }
+      }
+    })
+    if (user !== null && user !== undefined) {
+      return {
+        id: user.id,
+        name: user.name,
+        document: user.document,
+        email: user.email,
+        zipcode: user.addresses[0].zipcode,
+        number: user.addresses[0].number,
+        complements: user.addresses[0].complements,
+        mobilePhone: user.mobile_phone
+      }
+    }
+  }
+}
