d24d528b8b1889941d154ffd76dde542120bd478
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-12 13:29:19 -0300

    feat: add enums of transaction status and payment type

---
+++ src/domain/enums/transaction.ts
@@ -0,0 +1,15 @@
+export const TransactionStatus = {
+  STARTED: 'started',
+  PROCESSING: 'processing',
+  PENDING: 'pending',
+  APPROVED: 'approved',
+  REFUSED: 'refused',
+  REFUNDED: 'refunded',
+  CHARGEBACK: 'chargeback',
+  ERROR: 'error'
+}
+
+export const PaymentType = {
+  BILLET: 'billet',
+  CREDIT_CARD: 'credit_card'
+}
