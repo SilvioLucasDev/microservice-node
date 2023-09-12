59b45fd63d02059252c70c5fe6a6597bc462cf81
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-12 16:16:52 -0300

    test: ensure PgUserRepository return user if exists

---
+++ tests/infra/repositories/postgres/user.spec.ts
@@ -0,0 +1,44 @@
+import { prismaMock } from '@/tests/infra/repositories/postgres/mocks'
+import { PgUserRepository } from '@/infra/repositories/postgres'
+
+import { type Prisma, type User as UserPrisma } from '@prisma/client'
+
+describe('PgUserRepository', () => {
+  let id: string
+  let name: string
+  let document: string
+  let email: string
+  let mobilePhone: string
+  let zipcode: string
+  let number: string
+  let complements: string
+
+  let sut: PgUserRepository
+
+  beforeEach(() => {
+    id = 'any_id'
+    name = 'any_name'
+    document = 'any_document'
+    email = 'any_email'
+    mobilePhone = 'any_mobile_phone'
+    zipcode = 'any_zipcode'
+    number = 'any_number'
+    complements = 'any_complements'
+
+    sut = new PgUserRepository()
+  })
+
+  afterAll(async () => {
+    await prismaMock.$disconnect()
+  })
+
+  it('should return user if exists', async () => {
+    prismaMock.user.findFirst.mockResolvedValueOnce({
+      id, name, document, email, mobile_phone: mobilePhone, addresses: [{ zipcode, number, complements }]
+    } as unknown as Prisma.Prisma__UserClient<UserPrisma>)
+
+    const user = await sut.get({ id })
+
+    expect(user).toEqual({ id, name, document, email, mobilePhone, zipcode, number, complements })
+  })
+})
