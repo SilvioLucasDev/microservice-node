57f742e4e871e7a39e3c634db4d157032cbbf833
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-12 17:56:16 -0300

    refactor: apply factories makePgCardRepository and makePgUserRepository

--- src/main/factories/application/use-cases/process-payment.ts
+++ src/main/factories/application/use-cases/process-payment.ts
@@ -2,13 +2,15 @@ import { ProcessPaymentUseCase } from '@/application/use-cases'
 import { makeUUIDAdapter } from '@/main/factories/infra/adapters'
 import { makeRabbitMQAdapter } from '@/main/factories/infra/adapters/queue'
 import { makeFakePaymentGateway } from '@/main/factories/infra/gateways'
-import { makePgTransactionRepository } from '@/main/factories/infra/repositories/postgres'
+import { makePgCardRepository, makePgTransactionRepository, makePgUserRepository } from '@/main/factories/infra/repositories/postgres'
 
 export const makeProcessPaymentUseCase = (): ProcessPaymentUseCase => {
   return new ProcessPaymentUseCase(
+    makePgUserRepository(),
+    makePgCardRepository(),
+    makePgTransactionRepository(),
     makeFakePaymentGateway(),
     makeUUIDAdapter(),
-    makePgTransactionRepository(),
     makeRabbitMQAdapter()
   )
 }
--- src/main/factories/application/use-cases/process-ticket.ts
+++ src/main/factories/application/use-cases/process-ticket.ts
@@ -1,10 +1,11 @@
 import { ProcessTicketUseCase } from '@/application/use-cases'
-import { makePgTicketRepository } from '@/main/factories/infra/repositories/postgres'
+import { makePgTicketRepository, makePgUserRepository } from '@/main/factories/infra/repositories/postgres'
 import { makeRabbitMQAdapter } from '@/main/factories/infra/adapters/queue'
 
 export const makeProcessTicketUseCase = (): ProcessTicketUseCase => {
   return new ProcessTicketUseCase(
     makePgTicketRepository(),
+    makePgUserRepository(),
     makeRabbitMQAdapter()
   )
 }
