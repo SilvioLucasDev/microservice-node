05907f8bb2e43a2f810a4182a3e4030e0ee40894
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-12 13:35:12 -0300

    refactor: adjust event TicketReserved

--- src/domain/event/ticket-reserved.ts
+++ src/domain/event/ticket-reserved.ts
@@ -1,9 +1,10 @@
 export class TicketReserved {
   constructor (
+    private readonly paymentType: string,
+    private readonly price: number,
     private readonly ticketId: string,
-    private readonly eventId: string,
-    private readonly creditCardToken: string,
-    private readonly price: string,
-    private readonly email: string
+    private readonly userId: string,
+    private readonly cardId: string | null,
+    private readonly installments: number | null
   ) {}
 }
