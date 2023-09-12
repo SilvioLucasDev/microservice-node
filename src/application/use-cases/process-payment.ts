72b0c560916e2028fbaedd263e771752691c2386
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-12 13:08:04 -0300

    refactor: ensure ProcessPaymentUseCase calls CardRepository and UserRepository

--- src/application/use-cases/process-payment.ts
+++ src/application/use-cases/process-payment.ts
@@ -1,30 +1,42 @@
 import { type MakePayment } from '@/application/contracts/gateways'
 import { type Publish, type UUIDGenerator } from '@/application/contracts/adapters'
 import { Transaction } from '@/domain/entities'
-import { type SaveTransaction } from '@/application/contracts/repositories'
+import { type GetCard, type GetUser, type SaveTransaction } from '@/application/contracts/repositories'
 import { PaymentProcessed } from '@/domain/event'
+import { CardNotFoundError } from '@/application/errors'
+import { PaymentType } from '@/domain/enums'
 
 export class ProcessPaymentUseCase {
   constructor (
+    private readonly userRepository: GetUser,
+    private readonly cardRepository: GetCard,
+    private readonly transactionRepository: SaveTransaction,
     private readonly paymentGateway: MakePayment,
     private readonly crypto: UUIDGenerator,
-    private readonly transactionRepository: SaveTransaction,
     private readonly queue: Publish
   ) {}
 
-  async execute ({ ticketId, email, eventId, price, creditCardToken }: Input): Promise<void> {
-    const payment = await this.paymentGateway.makePayment({ email, creditCardToken, price })
-    const transaction = Transaction.create({ eventId, ticketId, tid: payment.tid, price, status: payment.status }, this.crypto)
+  async execute ({ ticketId, price, paymentType, userId, cardId, installments }: Input): Promise<void> {
+    const transaction = Transaction.create({ ticketId, paymentType, cardId, total: price, installments }, this.crypto)
+    const user = await this.userRepository.get({ id: userId }) // TODO Isso deve ser uma requisição HTTP para o service de usuários.
+    let card = null
+    if (paymentType === PaymentType.CREDIT_CARD && cardId !== null) {
+      card = await this.cardRepository.get({ id: cardId })
+      if (card === undefined) throw new CardNotFoundError()
+    }
+    const payment = await this.paymentGateway.makePayment({ transactionId: transaction.id, user, card, total: price, paymentType, installments, dueDate: transaction.dueDate })
+    transaction.update({ processorResponse: payment.processorResponse, transactionId: payment.transactionId, status: payment.status })
     await this.transactionRepository.save(transaction)
-    const paymentProcessed = new PaymentProcessed(ticketId, payment.status)
+    const paymentProcessed = new PaymentProcessed(transaction.paymentType, ticketId, payment.url, payment.status)
     await this.queue.publish({ queueName: 'paymentProcessed', data: paymentProcessed })
   }
 }
 
 type Input = {
+  paymentType: string
+  price: number
   ticketId: string
-  eventId: string
-  email: string
-  price: string
-  creditCardToken: string
+  userId: string
+  cardId: string | null
+  installments: number | null
 }
--- tests/application/use-cases/process-payment.spec.ts
+++ tests/application/use-cases/process-payment.spec.ts
@@ -1,82 +1,164 @@
 import { ProcessPaymentUseCase } from '@/application/use-cases'
 import { type MakePayment } from '@/application/contracts/gateways'
-import { type SaveTransaction } from '@/application/contracts/repositories'
+import { type GetCard, type GetUser, type SaveTransaction } from '@/application/contracts/repositories'
 import { type Publish, type UUIDGenerator } from '@/application/contracts/adapters'
 import { Transaction } from '@/domain/entities'
 import { PaymentProcessed } from '@/domain/event'
+import { CardNotFoundError } from '@/application/errors'
 
 import { mock, type MockProxy } from 'jest-mock-extended'
+import MockDate from 'mockdate'
 
 jest.mock('@/domain/event/payment-processed')
 
 describe('ProcessPaymentUseCase', () => {
   let ticketId: string
+  let transactionId: string
+  let price: number
+  let paymentType: string
+  let userId: string
+  let cardId: string
+  let installments: number
+  let dueDate: Date
+  let id: string
+  let name: string
+  let document: string
   let email: string
-  let eventId: string
+  let zipcode: string
+  let number: string
+  let complements: string
+  let mobilePhone: string
+  let alias: string
+  let token: string
   let tid: string
-  let price: string
-  let creditCardToken: string
+  let url: string
+  let processorResponse: string
   let status: string
+  let user: object
+  let card: object
 
   let sut: ProcessPaymentUseCase
+  let transaction: jest.SpyInstance
+  let userRepository: MockProxy<GetUser>
+  let cardRepository: MockProxy<GetCard>
+  let transactionRepository: MockProxy<SaveTransaction>
   let paymentGateway: MockProxy<MakePayment>
   let crypto: MockProxy<UUIDGenerator>
-  let transaction: jest.SpyInstance
-  let transactionRepository: MockProxy<SaveTransaction>
   let queue: MockProxy<Publish>
 
-  beforeAll(() => {
-    ticketId = 'any_ticket_id'
-    email = 'any_email'
-    eventId = 'any_event_id'
-    tid = 'any_tid'
-    price = 'any_price'
-    creditCardToken = 'any_credit_card_token'
-    status = 'approved'
+  function getDueDate (): Date {
+    const dueDate = new Date()
+    dueDate.setDate(dueDate.getDate() + 10)
+    return dueDate
+  }
 
-    paymentGateway = mock()
-    paymentGateway.makePayment.mockResolvedValue({ tid, status })
-    crypto = mock()
-    crypto.uuid.mockReturnValue(ticketId)
+  beforeAll(() => {
+    MockDate.set(new Date())
+
+    ticketId = 'any_ticket_id'
+    transactionId = 'any_transaction_id'
+    price = 300
+    paymentType = 'credit_card'
+    userId = 'any_user_id'
+    cardId = 'any_card_id'
+    installments = 3
+    dueDate = getDueDate()
+    id = 'any_id'
+    name = 'any_name'
+    document = 'any_document'
+    email = 'any_email'
+    zipcode = 'any_zipcode'
+    number = 'any_number'
+    complements = 'any_complements'
+    mobilePhone = 'any_mobile_phone'
+    alias = 'any_alias'
+    token = 'any_token'
+    tid = 'any_tid'
+    url = 'any_url'
+    processorResponse = 'any_processor_response'
+    status = 'approved'
+    user = { id, name, document, email, zipcode, number, complements, mobile_phone: mobilePhone }
+    card = { id, alias, token }
+
     transaction = jest.spyOn(Transaction, 'create')
+    userRepository = mock()
+    userRepository.get.mockResolvedValue({ id, name, document, email, zipcode, number, complements, mobile_phone: mobilePhone })
+    cardRepository = mock()
+    cardRepository.get.mockResolvedValue({ id, alias, token })
     transactionRepository = mock()
+    paymentGateway = mock()
+    paymentGateway.makePayment.mockResolvedValue({ transactionId: tid, status, url, processorResponse })
+    crypto = mock()
+    crypto.uuid.mockReturnValue(transactionId)
     queue = mock()
   })
 
   beforeEach(() => {
-    sut = new ProcessPaymentUseCase(paymentGateway, crypto, transactionRepository, queue)
+    sut = new ProcessPaymentUseCase(userRepository, cardRepository, transactionRepository, paymentGateway, crypto, queue)
   })
 
-  it('should call method makePayment of PaymentGateway with correct values', async () => {
-    await sut.execute({ ticketId, email, eventId, price, creditCardToken })
-
-    expect(paymentGateway.makePayment).toHaveBeenCalledWith({ email, creditCardToken, price })
-    expect(paymentGateway.makePayment).toHaveBeenCalledTimes(1)
+  afterAll(() => {
+    MockDate.reset()
   })
 
   it('should call TransactionEntity with correct values', async () => {
-    await sut.execute({ ticketId, email, eventId, price, creditCardToken })
+    await sut.execute({ ticketId, price, paymentType, userId, cardId, installments })
 
-    expect(transaction).toHaveBeenCalledWith({ eventId, ticketId, tid, price, status }, crypto)
+    expect(transaction).toHaveBeenCalledWith({ ticketId, paymentType, cardId, total: price, installments }, crypto)
     expect(transaction).toHaveBeenCalledTimes(1)
   })
 
+  it('should call UserRepository with correct values', async () => {
+    await sut.execute({ ticketId, price, paymentType, userId, cardId, installments })
+
+    expect(userRepository.get).toHaveBeenCalledWith({ id: userId })
+    expect(userRepository.get).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call CardRepository with correct values', async () => {
+    await sut.execute({ ticketId, price, paymentType, userId, cardId, installments })
+
+    expect(cardRepository.get).toHaveBeenCalledWith({ id: cardId })
+    expect(cardRepository.get).toHaveBeenCalledTimes(1)
+  })
+
+  it('should not call CardRepository if TypePayment is billet', async () => {
+    await sut.execute({ ticketId, price, paymentType: 'billet', userId, cardId: null, installments })
+
+    expect(cardRepository.get).not.toHaveBeenCalled()
+  })
+
+  it('should throw CardNotFoundError if CardRepository return undefined', async () => {
+    cardRepository.get.mockResolvedValueOnce(undefined)
+
+    const promise = sut.execute({ ticketId, price, paymentType, userId, cardId, installments })
+
+    await expect(promise).rejects.toThrow(new CardNotFoundError())
+  })
+
+  it('should call method makePayment of PaymentGateway with correct values', async () => {
+    await sut.execute({ ticketId, price, paymentType, userId, cardId, installments })
+
+    expect(paymentGateway.makePayment).toHaveBeenCalledWith({ transactionId, user, card, total: price, paymentType, installments, dueDate })
+    expect(paymentGateway.makePayment).toHaveBeenCalledTimes(1)
+  })
+
   it('should call TransactionRepository with instance of TransactionEntity', async () => {
-    await sut.execute({ ticketId, email, eventId, price, creditCardToken })
+    await sut.execute({ ticketId, price, paymentType, userId, cardId, installments })
 
     expect(transactionRepository.save).toHaveBeenCalledWith(expect.any(Transaction))
     expect(transactionRepository.save).toHaveBeenCalledTimes(1)
   })
 
   it('should call PaymentProcessed with correct values', async () => {
-    await sut.execute({ ticketId, email, eventId, price, creditCardToken })
+    await sut.execute({ ticketId, price, paymentType, userId, cardId, installments })
 
-    expect(PaymentProcessed).toHaveBeenCalledWith(ticketId, status)
+    expect(PaymentProcessed).toHaveBeenCalledWith(paymentType, ticketId, url, status)
     expect(PaymentProcessed).toHaveBeenCalledTimes(1)
   })
 
   it('should call QueueAdapter with correct values', async () => {
-    await sut.execute({ ticketId, email, eventId, price, creditCardToken })
+    await sut.execute({ ticketId, price, paymentType, userId, cardId, installments })
 
     expect(queue.publish).toHaveBeenCalledWith({ queueName: 'paymentProcessed', data: expect.any(PaymentProcessed) })
     expect(queue.publish).toHaveBeenCalledTimes(1)
