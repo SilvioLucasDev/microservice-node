d60c3d0f3ea58e616bfe3a46b52171d1d5de7f4b
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-13 00:57:34 -0300

    test: ensure AxiosClient calls axios get method with correct values

---
+++ tests/infra/adapters/axios.spec.ts
@@ -0,0 +1,37 @@
+import { AxiosClient } from '@/infra/adapters'
+
+import axios from 'axios'
+
+jest.mock('axios')
+
+describe('AxiosClient', () => {
+  let url: string
+  let params: object
+
+  let sut: AxiosClient
+  let axiosMock: jest.Mocked<typeof axios>
+
+  beforeAll(() => {
+    url = 'any_url'
+    params = { any: 'any' }
+
+    axiosMock = axios as jest.Mocked<typeof axios>
+    axiosMock.get.mockResolvedValue({
+      status: 200,
+      data: 'any_data'
+    })
+  })
+
+  beforeEach(() => {
+    sut = new AxiosClient()
+  })
+
+  describe('get', () => {
+    it('should call method get of axios with correct values', async () => {
+      await sut.get({ url, params })
+
+      expect(axiosMock.get).toHaveBeenCalledWith(url, { params })
+      expect(axiosMock.get).toHaveBeenCalledTimes(1)
+    })
+  })
+})
