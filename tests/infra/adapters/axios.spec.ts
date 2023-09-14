c0fc310fe449f428797144a3d675af2476c5f0e7
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-13 23:26:01 -0300

    test: add tests of interface PostClient in AxiosAdapter

--- tests/infra/adapters/axios.spec.ts
+++ tests/infra/adapters/axios.spec.ts
@@ -1,50 +1,82 @@
-import { AxiosClient } from '@/infra/adapters'
+import { AxiosAdapter } from '@/infra/adapters'
 
 import axios from 'axios'
 
 jest.mock('axios')
 
-describe('AxiosClient', () => {
+describe('AxiosAdapter', () => {
   let url: string
+  let headers: object
   let params: object
+  let data: object
 
-  let sut: AxiosClient
+  let sut: AxiosAdapter
   let axiosMock: jest.Mocked<typeof axios>
 
   beforeAll(() => {
     url = 'any_url'
+    headers = { any: 'any' }
     params = { any: 'any' }
+    data = { any: 'any' }
 
     axiosMock = axios as jest.Mocked<typeof axios>
     axiosMock.get.mockResolvedValue({
       status: 200,
       data: 'any_data'
     })
+
+    axiosMock.post.mockResolvedValue({
+      status: 200,
+      data: 'any_data'
+    })
   })
 
   beforeEach(() => {
-    sut = new AxiosClient()
+    sut = new AxiosAdapter()
   })
 
   describe('get', () => {
     it('should call method get of axios with correct values', async () => {
-      await sut.get({ url, params })
+      await sut.get({ url, params, headers })
 
-      expect(axiosMock.get).toHaveBeenCalledWith(url, { params })
+      expect(axiosMock.get).toHaveBeenCalledWith(url, { headers, params })
       expect(axiosMock.get).toHaveBeenCalledTimes(1)
+    })
+
+    it('should return data on success', async () => {
+      const result = await sut.get({ url, params, headers })
+
+      expect(result).toEqual('any_data')
+    })
+
+    it('should rethrows if get throws', async () => {
+      axiosMock.get.mockRejectedValueOnce(new Error('http_error'))
+
+      const promise = sut.get({ url, params, headers })
+
+      await expect(promise).rejects.toThrow(new Error('http_error'))
     })
   })
 
-  it('should return data on success', async () => {
-    const result = await sut.get({ url, params })
+  describe('post', () => {
+    it('should call method post of axios with correct values', async () => {
+      await sut.post({ url, data, headers })
 
-    expect(result).toEqual('any_data')
-  })
+      expect(axiosMock.post).toHaveBeenCalledWith(url, data, { headers })
+      expect(axiosMock.post).toHaveBeenCalledTimes(1)
+    })
 
-  it('should rethrows if get throws', async () => {
-    axiosMock.get.mockRejectedValueOnce(new Error('http_error'))
-    const promise = sut.get({ url, params })
+    it('should return data on success', async () => {
+      const result = await sut.post({ url, data, headers })
 
-    await expect(promise).rejects.toThrow(new Error('http_error'))
+      expect(result).toEqual('any_data')
+    })
+
+    it('should rethrows if post throws', async () => {
+      axiosMock.post.mockRejectedValueOnce(new Error('http_error'))
+      const promise = sut.post({ url, data, headers })
+
+      await expect(promise).rejects.toThrow(new Error('http_error'))
+    })
   })
 })
