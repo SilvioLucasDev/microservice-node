86c6c7cb2baeeb026d2b1c2a4d2be29ec9a80f81
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-14 18:55:08 -0300

    feat: add interface UpdateStatusTransaction of TransactionRepository

--- src/application/contracts/repositories/transaction.ts
+++ src/application/contracts/repositories/transaction.ts
@@ -7,3 +7,14 @@ export interface SaveTransaction {
 export namespace SaveTransaction {
   export type Input = Transaction
 }
+
+export interface UpdateStatusTransaction {
+  updateStatus: (input: UpdateStatusTransaction.Input) => Promise<void>
+}
+
+export namespace UpdateStatusTransaction {
+  export type Input = {
+    id: string
+    status: string
+  }
+}
