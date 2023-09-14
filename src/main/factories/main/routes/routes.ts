462a1ce64051ed7cf433a4afd6034e3bc06cdff1
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-14 19:56:11 -0300

    feat: add in makeHttpServer factory WebHookRouter

--- src/main/factories/main/routes/routes.ts
+++ src/main/factories/main/routes/routes.ts
@@ -1,10 +1,10 @@
-import { CardRouter, TicketRouter } from '@/main/routes'
+import { CardRouter, TicketRouter, WebHookRouter } from '@/main/routes'
 import { makeExpressAdapter } from '@/main/factories/presentation/adapters'
 
 export const makeHttpServer = (): void => {
   const httpServer = makeExpressAdapter()
   new TicketRouter(httpServer)
   new CardRouter(httpServer)
-
+  new WebHookRouter(httpServer)
   httpServer.listen()
 }
